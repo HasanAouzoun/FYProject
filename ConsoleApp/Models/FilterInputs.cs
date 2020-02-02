2eb42a90eec48f2f2dc0f7e3272c773181641d21
HasanAouzoun <hasanozon4@gmail.com>
2020-02-02 23:59:20 +0000

    Created FilterInputs Model

---
+++ ConsoleApp/Models/FilterInputs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp.Models
+{
+    class FilterInputs
+    {
+        public Regex Pattern { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool ReverseFilter { get; set; }
+    }
+}
