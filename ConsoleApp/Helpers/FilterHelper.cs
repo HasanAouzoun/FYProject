0ec2b86cb288e8b0b1019216cdc095e920ed17f7
HasanAouzoun <hasanozon4@gmail.com>
2020-02-03 01:32:02 +0000

    Created a filter output model and a filter functionality

---
+++ ConsoleApp/Helpers/FilterHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using ConsoleApp.Models;
+
+namespace ConsoleApp.Helpers
+{
+    class FilterHelper
+    {
+        public static FilterOutputs FilterList(List<string> list, Regex regex)
+        {
+            // set up for feedback (progress)
+            var lenght = list.Count;
+            Console.CursorVisible = false;
+            var rowNumber = Console.CursorTop;
+
+            var filteredList = new List<string>();
+            var removedList = new List<string>();
+
+            for (int i = 0; i < lenght; i++)
+            {
+                string str = list[i];
+                // reset the cursor possition to the beginning of the current line
+                Console.SetCursorPosition(0, rowNumber);
+
+                if (regex.IsMatch(str))
+                {
+                    filteredList.Add(str);
+                }
+                else
+                {
+                    removedList.Add(str);
+                }
+
+                // display progress
+                var progress = (double)(i + 1) / lenght * 100;
+                Console.WriteLine($"Filtering List ... ({string.Format("{0:0.00}", progress)}%)");
+            }
+
+            // display cursor when done
+            Console.CursorVisible = true;
+
+            return new FilterOutputs { FilteredList = filteredList, RemovedList = removedList };
+        }
+    }
+}
---
+++ ConsoleApp/Models/FilterOutputs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp.Models
+{
+    class FilterOutputs
+    {
+        public List<string> FilteredList { get; set; }
+        public List<string> RemovedList { get; set; }
+    }
+}
