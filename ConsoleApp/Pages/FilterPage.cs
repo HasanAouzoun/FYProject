458bf4e991f43059aecfbf101ff8c6bbb1676854
HasanAouzoun <hasanozon4@gmail.com>
2020-02-03 00:00:56 +0000

    Done Filter Inputs request

--- ConsoleApp/Pages/FilterPage.cs
+++ ConsoleApp/Pages/FilterPage.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
-using System.Configuration;
+using System.IO;
 using System.Text;
+using ConsoleApp.Helpers;
+using ConsoleApp.Models;
 
 namespace ConsoleApp.Pages
 {
@@ -24,29 +26,127 @@ namespace ConsoleApp.Pages
                 "\n\tReverse: it will return the removed list. i.e. the list that did not match the filter pattern.");
             Console.WriteLine();
 
-            // Inputs
-            Console.CursorVisible = true;
-            Console.WriteLine("### Filter Inputs ###");
-            RequestValidateRegex();
+            // Request Inputs
+            var inputs = RequestInputs();
+
+            // Process Filter
+            // create Folder/ Directory (title)
+            // create the Description.txt
+            // process the filter
+            
+        
         }
 
-        public static Regex RequestValidateRegex()
+        private static FilterInputs RequestInputs()
         {
+            // Request Inputs
+            Console.CursorVisible = true;
+            Console.WriteLine("### Filter Inputs ###");
+            var regex = RequestPattern();
+            var title = RequestTitle();
+            var description = RequestDescription();
+            var filterType = RequestFilterType();
+            Console.WriteLine();
+
+            // Confirm Inputs
+            // if confirmation failed then reset the Filter Request
+            Console.WriteLine("### Input Confirmation ###");
+            Console.WriteLine($"\tPattern: {regex.ToString()}" +
+                $"\n\tTitle: {title}" +
+                $"\n\tDescription: {description}" +
+                $"\n\tReverseType: {filterType}");
+
+            if (ConsoleHelper.InputConfirmation() == false)
+            {
+                return RequestInputs();
+            }
+
+            return new FilterInputs
+            {
+                Pattern = regex,
+                Title = $"{title} - {DateTime.Now.ToString("dd/MM/yyyy H-mm tt")}",
+                Description = description,
+                ReverseFilter = filterType
+            };
+        }
+
+        private static Regex RequestPattern()
+        {
+            // Request Pattern
             Console.Write("Filter Pattern: ");
             var pattern = Console.ReadLine();
 
-            if()
+            if (string.IsNullOrEmpty(pattern))
+            {
+                Console.WriteLine("Pattern can not be empty.");
+                return RequestPattern();
+            }
 
-            Console.WriteLine(pattern);
+            // Confirm pattern
+            if(ConsoleHelper.InputConfirmation() == false)
+            {
+                return RequestPattern();
+            }
 
-            return new Regex("");
+            // Create/Check pattern
+            try
+            {
+                Console.WriteLine("Pattern is valid.");
+                return new Regex(pattern);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message.ToString()}");
+                return RequestPattern();
+            }
         }
 
-        public static void ValidateRegexString(string pattern)
+        private static string RequestTitle()
         {
-            var testString = "someone@example.com";
+            // request Title
+            Console.Write("Title: ");
+            var title = Console.ReadLine();
 
-            var validator = new RegexStringValidator(pattern);
+            if (string.IsNullOrEmpty(title))
+            {
+                Console.WriteLine("Title can not be empty.");
+                return RequestTitle();
+            }
+
+            return title;
+        }
+
+        private static string RequestDescription()
+        {
+            // request Description
+            Console.Write("Description: ");
+            var description = Console.ReadLine();
+
+            return description;
+        }
+
+        private static bool RequestFilterType()
+        {
+            // request type
+            Console.Write("Type -- please type (Normal (n) or Reverse (r)): ");
+            var type = Console.ReadLine();
+            
+            if( type.ToLower().Equals("normal") || type.ToLower().Equals("n") )
+            {
+                // return false for (ReverseFilter)
+                return false;
+            }
+            else if (type.ToLower().Equals("reverse") || type.ToLower().Equals("r"))
+            {
+                // return true for (ReverseFilter)
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Wrong type");
+                return RequestFilterType();
+            }
+        }
         }
 
         public static List<string> Filter(List<string> list, string regex, string title, string description, bool reverseFilter)
